0b77570ddd8b8077ae6ba752f06caafbbafeb352
Samuel <samuel.natel20@gmail.com>
2025-11-01 16:11:25 -0300

    Criação de pastas e arquivos PHP

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database
+{
+    private static $host = 'localhost';
+    private static $db_name = 'digitaco';
+    private static $username = 'root';
+    private static $password = '{senha do seu banco aqui}';
+    private static $conn;
+
+    public static function connect()
+    {
+        if (!self::$conn)
+        {
+            self::$conn = new mysqli(self::$host, self::$username, self::$password, self::$db_name);
+            if (self::$conn->connect_error)
+            {
+                die(json_encode(["error" => "Conexão falhou: " . self::$conn->connect_error]));
+            }
+        }
+        return self::$conn;
+    }
+}
