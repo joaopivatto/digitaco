f0ebfd2a22aab2bb839d809318bff748edc1dd46
Joao Pivatto <jgpivatto@gmail.com>
2025-11-03 20:48:54 -0300

    feat: implementando create matches e historico sem validacao de user

--- backend/dao/MatchesDAO.php
+++ backend/dao/MatchesDAO.php
@@ -2,7 +2,65 @@
 
 namespace backend\dao;
 
+require_once "/../../config/Database.php";
+require_once "/../../dto/MessageResponseDTO.php";
+
+use backend\config\Database;
+use backend\dto\matches\MatchesDTO;
+use backend\dto\matches\MatchesHistoryDTO;
+use dto\MessageResponseDTO;
+
 class MatchesDAO
 {
+
+    public static function create(int $points, int $words, int $leagueId, int $userId): MessageResponseDTO {
+        $conn = Database::connect();
+        $sql = $conn->prepare("INSERT INTO matches (points, words, league_id, user_id) VALUES (?,?,?, ?)");
+        $sql->bind_param("iiii", $points, $words, $leagueId, $userId);
+        $sql->execute();
+
+        return new MessageResponseDTO("Partida finalizada!", 201);
+    }
+
+    public static function getHistoric(int $userId): MessageResponseDTO {
+        $conn = Database::connect();
+        $sql = $conn->prepare("
+            SELECT 
+              COUNT(*) AS totalMatches,
+              SUM(words) AS totalWords,
+              SUM(points) AS totalPoints,
+              MAX(points) AS bestScore
+            FROM matches
+            WHERE user_id = ?;
+        ");
+        $sql->bind_param("i", $userId);
+        $sql->execute();
+        $res = $sql->get_result();
+        $historic = new MatchesHistoryDTO();
+        if ($res && $row=$res->fetch_assoc()) {
+            $historic->setTotalMatches($row['totalMatches']);
+            $historic->setTotalWords($row['totalWords']);
+            $historic->setTotalPoints($row['totalPoints']);
+            $historic->setBestScore($row['bestScore']);
+        }
+
+        $sql = $conn->prepare("
+            SELECT points, words, played_at
+            FROM matches
+            WHERE user_id = ?
+            ORDER BY played_at DESC;
+        ");
+        $sql->bind_param("i", $userId);
+        $sql->execute();
+        $res = $sql->get_result();
+        $matches = [];
+        if ($res && $row=$res->fetch_assoc()) {
+            $match = new MatchesDto($row['points'], $row['words'], $row['played_at']);
+            $matches[] = $match->jsonSerialize();
+        }
+        $historic->setMatches($matches);
+
+        return $historic;
+    }
 
 }
---
+++ backend/dto/matches/MatchesDTO.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace backend\dto\matches;
+
+
+class MatchesDTO
+{
+    private int $points;
+
+    private int $words;
+    private string $date;
+
+    public function __construct(int $points, int $words, $date) {
+        $this->points = $points;
+        $this->words = $words;
+        $this->date = new \DateTime($date)->format('d/m/Y H:i');
+    }
+
+    public function jsonSerialize(): array {
+        return [
+            "points"=>$this->points,
+            "words"=>$this->words,
+            "date"=>$this->date
+        ];
+    }
+
+    public function getPoints(): int
+    {
+        return $this->points;
+    }
+
+    public function setPoints(int $points): void
+    {
+        $this->points = $points;
+    }
+
+    public function getWords(): int
+    {
+        return $this->words;
+    }
+
+    public function setWords(int $words): void
+    {
+        $this->words = $words;
+    }
+
+    public function getDate(): string
+    {
+        return $this->date;
+    }
+
+    public function setDate(string $date): void
+    {
+        $this->date = $date;
+    }
+
+
+}
