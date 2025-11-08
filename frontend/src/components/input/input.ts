ce6a75ffe306a4ba4ef0bd24b2a93767b0ef05bb
JVDourado10 <jovneiva@gmail.com>
2025-11-08 13:17:31 -0300

    feat: Login page

---
+++ frontend/src/components/input/input.ts
@@ -0,0 +1,48 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-input',
+  imports: [],
+  templateUrl: './input.html',
+  styleUrl: './input.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
+})
+export class InputComponent implements ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() type: string = 'text';
+  @Input() required: boolean = true;
+
+  internalValue: string = '';
+  disabled: boolean = false;
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  writeValue(value: any): void {
+    this.internalValue = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  onInternalInput(event: Event): void {
+    const value = (event.target as HTMLInputElement).value;
+    this.internalValue = value;
+    this.onChange(this.internalValue);
+    this.onTouched();
+  }
+}
--- frontend/src/pages/login/login.ts
+++ frontend/src/pages/login/login.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../../components/input/input';
+import { Button } from '../../components/button/button';
+
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [InputComponent, FormsModule, Button],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  label: string = 'Texto';
 
+  loginData = {
+    email: '',
+    senha: ''
+  };
+
+  onSubmit() {
+    console.log(this.loginData);
+  }
 }
